b56958201bf04be98a6519a24ccf11465ed91827
xtuxur <alex.delzar@gmail.com>
2017-10-04 18:29:32 -0500

    DB to BD

--- Program.cs
+++ Program.cs
@@ -7,34 +7,36 @@ namespace lista
     {
         static void Main(string[] args)
         {
-            using (var db = new ListaContext())
+            
+            ///Crea una conexión con la BD
+            using (var bd = new ListaContext())
             {
 
                 Console.WriteLine("Ingresa un comando (+ Elemento, - Elemento, o -- para eliminar la lista)):");
 
                 /// Se asegura que la BD esté creada
-                db.Database.EnsureCreated();
+                bd.Database.EnsureCreated();
 
 
                 /// Busca y regresa el primer el elemento en la BD que cumpla con la condición
                 /// si no lo encuentra devuelve null
-                var e = db.Elementos.FirstOrDefault(x => x.Nombre == "PLATANOS");
+                var e = bd.Elementos.FirstOrDefault(x => x.Nombre == "PLATANOS");
 
 
                 /// Si no es nulo, quiere decir que si encontró el elemento y lo puede borrar
                 if (e != null)
                 {
                     ///Borra dicho elemento de la BD
-                    db.Elementos.Remove(e);
+                    bd.Elementos.Remove(e);
                 }
 
                 /// Agrega un nuevo elemento a la table "Elementos" con los datos proporcionados
                 /// db.NombreDeLaTabla.Add
-                db.Elementos.Add(new Elemento { Nombre = "Platanos", Agregado = DateTime.Now });
+                bd.Elementos.Add(new Elemento { Nombre = "Platanos", Agregado = DateTime.Now });
 
                 /// Guarda los cambios en la base de datos
                 /// pasando este momento se reflejan los cambios en la BD
-                var count = db.SaveChanges();
+                var count = bd.SaveChanges();
 
                 Console.WriteLine("{0} registros guardados en la base de datos", count);
 
@@ -44,12 +46,12 @@ namespace lista
 
                 /// Regresa todos los elementos en la tabla especificada en este caso Elementos
                 /// db.NombreDeLaTabla
-                var ListaElementos = db.Elementos;
+                var ListaElementos = bd.Elementos;
 
 
 
 
-                count = db.SaveChanges();
+                count = bd.SaveChanges();
                 Console.WriteLine($"Se eliminó el siguiente elemento: {e} ");
 
 
